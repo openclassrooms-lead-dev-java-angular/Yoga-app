3837c776fc0b8c3480382e489d9dfa967b294ea6
Frederic Mas <fredfredfredm@yahoo.fr>
2026-08-12 11:33:21 +0200

    fix(app/typing): fix any types

--- front/src/app/pages/sessions/components/detail/detail.component.ts
+++ front/src/app/pages/sessions/components/detail/detail.component.ts
@@ -42,17 +42,17 @@ export class DetailComponent implements OnInit {
     this.fetchSession();
   }
 
-  public back() {
+  public back(): void {
     window.history.back();
   }
 
   public delete(): void {
     this.sessionApiService
       .delete(this.sessionId)
-      .subscribe((_: any) => {
-          this.matSnackBar.open('Session deleted !', 'Close', { duration: 3000 });
-          this.router.navigate(['sessions']);
-        }
+      .subscribe(() => {
+        this.matSnackBar.open('Session deleted !', 'Close', { duration: 3000 });
+        this.router.navigate(['sessions']);
+      }
       );
   }
 
