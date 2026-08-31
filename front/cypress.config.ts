7dcdffab010f597dcb8324d67a23b40bdc12821a
Frederic Mas <fredfredfredm@yahoo.fr>
2026-08-31 14:45:36 +0200

    fix(app/cypress): update cypressconfig and remove index plugin

--- front/cypress.config.ts
+++ front/cypress.config.ts
@@ -1,16 +1,20 @@
 import { defineConfig } from 'cypress'
+import codeCoverageTask from '@cypress/code-coverage/task'
 
 export default defineConfig({
-  videosFolder: 'cypress/videos',
-  screenshotsFolder: 'cypress/screenshots',
-  fixturesFolder: 'cypress/fixtures',
   video: false,
+
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts').default(on, config)
-    },
     baseUrl: 'http://localhost:4200',
+
+    fixturesFolder: 'cypress/fixtures',
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
+
+    setupNodeEvents(on, config) {
+      codeCoverageTask(on, config)
+
+      return config
+    },
   },
 })
